8fd0af27038cd0c2aec433c4b591b4bb78e769b7
aransaibhang <aran@saibhang.io>
2023-07-07 14:50:25 +0530

    footer also added

---
+++ src/Components/Ui/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../Styles/Footer.css"
+export default function Footer() {
+  return (
+    <footer id="footer">
+    <div className="container">
+      <h3>Aranpreet Singh Saini</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, quisquam!.</p>
+      <div className="social-links">
+        <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
+        <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+        <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+        <a href="#" className="google-plus"><i className="bi bi-skype"></i></a>
+        <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
+  )
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import Carousel from '../../Components/Ui/Carousel'
 import Services from '../../Components/Ui/Services'
 import Testimonials from '../../Components/Ui/Testimonials'
 import Team from '../../Components/Ui/Team'
+import Footer from '../../Components/Ui/Footer'
 export default function Home() {
   return (
     <div><Carousel/>
 <div className='container' style={{paddingLeft:"7rem",paddingRight:"7rem"}}><Services/>
     <Testimonials/>
-    <Team/></div>
+    <Team/>
+    <Footer/></div>
     </div>
   )
 }
